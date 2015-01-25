0af27fefab0e2b1ae0108fbf91fb0022f9388f4a
Todd Wolfson <todd@twolfson.com>
2015-01-24 19:14:22 -0600

    Set up a better build chain

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,31 @@
 module.exports = function(grunt) {
   // Configure our tasks
   grunt.initConfig({
+    // For our build, clean the build directory
+    clean: {
+      build: ['tmp-build/']
+    },
+
+    // For our build, copy the entire directory
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: ['lib/**/*'],
+          dest: 'tmp-build/'
+        }]
+      }
+    },
+
+    // For our build, compile browserify files
+    browserify: {
+      build: {
+        files: {
+          'tmp-build/chrome/content/pinboardff.js': ['lib/chrome/content/pinboardff.js']
+        }
+      }
+    },
+
     // Build for distribution
     zip: {
       build: {
@@ -9,14 +34,14 @@ module.exports = function(grunt) {
         dest: 'dist/firefox-pinboard.xpi'
       }
     }
+
+    // During development, watch our files and trigger a re-upload via a push
   });
 
   // Load in grunt tasks
   require('load-grunt-tasks')(grunt);
 
-  // Create task for getting original
-  grunt.registerTask('install-original', ['curl', 'copy', 'unzip']);
-
   // Create task for building a distributable file
-  grunt.registerTask('build', ['zip']);
+  grunt.registerTask('build', ['clean:build', 'copy:build', 'browserify:build', 'zip']);
+  grunt.registerTask('dev', ['watch:dev']);
 };
